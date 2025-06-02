edb7980b7ec1bbe9966e85bba197beda9cd587d1
ilaafshaikhgit config --global user.name ilaafshaikh <ilaafshaikh61@gmail.com>
2025-06-02 18:43:15 +0530

    second commit

--- array.java
+++ array.java
@@ -2,13 +2,6 @@
 
 import java.util.Scanner;
 public class array8{
-	// public static void update(int marks[]){
-    //     for(int i=0;i<marks.length;i++){
-    //         //1.arrays are call by reference
-    //         marks[i]=marks[i]+1;
-
-    //     }
-    // }
     // public static int linearsearch(int num[],int key){
     //     for(int i=0;i<num.length;i++){
     //     if(num[i] == key){
