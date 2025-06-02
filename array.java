10d24af468719567a4662c5d6517ce2b59e51063
ilaafshaikhgit config --global user.name ilaafshaikh <ilaafshaikh61@gmail.com>
2025-06-02 18:39:29 +0530

    second commit

--- array.java
+++ array.java
@@ -2,3 +2,10 @@
 
 import java.util.Scanner;
 public class array8{
+	// public static void update(int marks[]){
+    //     for(int i=0;i<marks.length;i++){
+    //         //1.arrays are call by reference
+    //         marks[i]=marks[i]+1;
+
+    //     }
+    // }
